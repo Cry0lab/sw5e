e423e6c68915fd442a0e7f1d3d9923c382014459
Cry0lab <dan-molina@outlook.com>
2023-08-20 14:23:23 -0400

    stuff

--- classes/DND_Tools.py
+++ classes/DND_Tools.py
@@ -1,3 +1,6 @@
+import requests
+import Equipment
+import Enhanced
 import random
 
 class DND_Tools:
@@ -25,3 +28,67 @@ class DND_Tools:
     
     def get_random_choice(self,item_list):
         return random.choice(item_list)
+
+    def get_cr_level(self,cr):
+        cr_level = 0
+        if cr > 0 and cr <= 4:
+            cr_level = 0
+        elif cr > 4 and cr <= 10:
+            cr_level = 1
+        elif cr > 10 and cr <= 16:
+            cr_level = 2
+        elif cr > 16:
+            cr_level = 3
+        
+        return cr_level
+
+    def get_roll_level(self):
+        roll_level = 0
+        roll = self.dice_roll(1, 100)
+        
+        if roll < 31:
+            roll_level = 0
+        elif roll < 61:
+            roll_level = 1
+        elif roll < 71:
+            roll_level = 2
+        elif roll < 96:
+            roll_level = 3
+        else:
+            roll_level = 4
+
+        return roll_level
+
+    def loot_credits_mob(self,cr):
+        credits = 0
+        
+        cr_1_4 = [self.dice_roll(1, 3),self.dice_roll(4, 6),(self.dice_roll(3, 6)*5),((self.dice_roll(3, 6))*10),((self.dice_roll(1, 6))*100)]
+        cr_5_10 = [self.dice_roll(9, 6)*10,self.dice_roll(5, 6)*50,self.dice_roll(7, 6)*100,self.dice_roll(4, 6)*100,self.dice_roll(5, 6)*100]
+        cr_11_16 = [self.dice_roll(7, 6)*200,self.dice_roll(3, 6)*500,self.dice_roll(3, 6)*1000,self.dice_roll(3, 6)*1000,self.dice_roll(3, 6)*1000]
+        cr_17_plus = [self.dice_roll(4, 6)*2000,self.dice_roll(2, 6)*6000,self.dice_roll(2, 6)*7000,self.dice_roll(2, 6)*10000,self.dice_roll(3, 6)*10000]
+        cr_table = [cr_1_4,cr_5_10,cr_11_16,cr_17_plus]
+        
+        
+        credit_table = cr_table[self.get_cr_level(cr)]
+        #print(credit_table)
+        credit_loot = credit_table[self.get_roll_level()]
+        return credit_loot
+
+    def loot_item_mob(self,cr,party_level):
+        e1 = Enhanced.Enhanced()
+        cr_level = self.get_cr_level(cr)
+        roll_level = self.get_roll_level()
+        artifact_toggle = False
+        if roll_level > 1 and cr_level < 3:
+            artifact_toggle = True
+            #loot = enchanced_item_list_generator(EnhancedItemsData, party_level,True)
+            #equipment_printer_random_enhanced(1, 1, loot, "Loot", party_level)
+        loot = e1.get_enhanced_loot(party_level,artifact_toggle)
+    #def loot_mob(self,cr,party_level):
+        
+
+dnd = DND_Tools()
+#creds = dnd.loot_credits_mob(4)
+#print(creds)
+item = dnd.loot_item_mob(4,4)
+print(item)
--- classes/Enhanced.py
+++ classes/Enhanced.py
@@ -1,7 +1,8 @@
 import requests
+import Equipment
+import random
 
-class Enchanced:
-
+class Enhanced:
 
     def get_all(self):
             # Make a GET request to the API endpoint
@@ -23,7 +24,6 @@ class Enchanced:
             enhanced_by_type[type].append(enhanced)
         return enhanced_by_type
 
-
     def get_types(self):
         types = []
         data = self.get_all()
@@ -31,16 +31,19 @@ class Enchanced:
             types.append(key)
         return types
 
-    def get_enchanced_from_type(self,specific_type):
-        enchanced = self.get_all()
-        specific_enchanced = []
-        for type, enchanced_list in enchanced.items():
-            if type == specific_type:
-                for item in enchanced_list:
-                    specific_enchanced.append(item)
-        return specific_enchanced 
+    def get_random_choice(self,item_list):
+        return random.choice(item_list)
 
-    def get_enchanced_of_rarity(self,rarity,enhanced_list):
+    def get_enhanced_from_type(self,specific_type):
+        enhanced = self.get_all()
+        specific_enhanced = []
+        for type, enhanced_list in enhanced.items():
+            if type == specific_type:
+                for item in enhanced_list:
+                    specific_enhanced.append(item)
+        return specific_enhanced 
+
+    def get_enhanced_of_rarity(self,rarity,enhanced_list):
         rarities = ["Blank", "Standard", "Premium", "Prototype", "Advanced", "Legendary", "Artifact"]
         rarity_value = rarities.index(rarity)
         specific_enhanced = []
@@ -52,20 +55,109 @@ class Enchanced:
                 specific_enhanced.append(item)
         return specific_enhanced
 
-    def get_enchanced_of_rarity_or_less(self,rarity,enhanced_list):
+    def get_enhanced_of_rarity_or_less(self,rarity,enhanced_list):
         rarities = ["Blank", "Standard", "Premium", "Prototype", "Advanced", "Legendary", "Artifact"]
         rarity_value = rarities.index(rarity)
+        #print(rarity_value)
         specific_enhanced = []
         #print(rarity_value)
         for item in enhanced_list:
-            #print(item["rarityOptionsEnum"])
+            #print(item["rarityOptionsEnum"][0])
             if item["rarityOptionsEnum"][0] <= rarity_value:
-                print(item["rarityOptionsEnum"])
+                #print(item["rarityOptionsEnum"])
                 specific_enhanced.append(item)
         return specific_enhanced
 
-e1 = Enchanced()
+    def get_rarity_level(self,party_level):
+        if party_level in range(1,5):
+            return "Standard"
+        elif party_level in range(5,9):
+            return "Premium"
+        elif party_level in range(9,13):
+            return "Prototype"
+        elif party_level in range(13,17):
+            return "Advanced"
+        else:
+            return "Legendary"
+    
+    def get_enhanced_cost(self,item):
+        cost = 0
+
+        roll1 = random.randint(1,100)
+        roll2 = random.randint(1,100)
+        if roll1 > roll2:
+            roll = roll1
+        else:
+            roll = roll2
+
+        if item["searchableRarity"] == "Standard":
+            cost = roll * 10
+        elif item["searchableRarity"] == "Premium":
+            cost = roll * 50
+        elif item["searchableRarity"] == "Prototype":
+            cost = roll * 250
+        elif item["searchableRarity"] == "Advanced":
+            cost = roll * 1000
+        elif item["searchableRarity"] == "Legendary":
+            cost = roll * 5000
+        elif item["searchableRarity"] == "Artifact":
+            cost = roll * 25000
+        return cost
+    
+    def get_black_market_enhanced_cost(self,item):
+        cost = 0
+
+        roll1 = DND.dice_roll(1,100)
+        roll2 = DND.dice_roll(1,100)
+        if roll1 > roll2:
+            roll = roll1
+        else:
+            roll = roll2
+
+        if item["searchableRarity"] == "Standard":
+            cost = roll * 15
+        elif item["searchableRarity"] == "Premium":
+            cost = roll * 60
+        elif item["searchableRarity"] == "Prototype":
+            cost = roll * 300
+        elif item["searchableRarity"] == "Advanced":
+            cost = roll * 1200
+        elif item["searchableRarity"] == "Legendary":
+            cost = roll * 7000
+        elif item["searchableRarity"] == "Artifact":
+            cost = roll * 30000
+        return cost
+
+    def format_enhanced_item(self,item,black_market_toggle):
+        if black_market_toggle == True:
+            cost = self.get_black_market_enhanced_cost(item)
+        else:
+            cost = self.get_enhanced_cost(item)
+        formatted_item = str(item["name"]).replace(',','') + "," + item["type"] + "," + item["searchableRarity"] + "," + str(cost)
+        return formatted_item
+
+    def format_random_enhanced_item(self,item_list,black_market_toggle):
+        item = self.get_random_choice(item_list)
+        formatted = self.format_enhanced_item(item,black_market_toggle)
+        return formatted
+
+    def get_enhanced_loot(self,party_level,artifact_toggle):
+        rarity = self.get_rarity_level(party_level)
+        d20 = random.randint(1,20)
+        if rarity == "Legendary":
+            if (d20 > 10 and artifact_toggle == True):
+                rarity = "Artifact"
+        item_list = self.get_enhanced_of_rarity_or_less(rarity,self.get_all())
+
+
+
+
+
+e1 = Enhanced()
 #print(e1.get_types())
-weapons = e1.get_enchanced_from_type("Weapon")
+weapons = e1.get_enhanced_from_type("Weapon")
 #print(weapons)
-premium_weapons = e1.get_enchanced_of_rarity_or_less("Premium", weapons)
+premium_weapons = e1.get_enhanced_of_rarity_or_less("Artifact", weapons)
+#print(premium_weapons[1]["searchableRarity"])
+#cost = e1.get_enhanced_cost(premium_weapons[1])
+#print(e1.format_random_enhanced_item(premium_weapons,False))
